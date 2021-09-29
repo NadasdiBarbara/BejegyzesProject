0c2acd9fac972c73bda516f914e1f8a6ad01ad9e
Nádasdi Barbara <nadasdibarbara15@gmail.com>
2021-09-29 16:20:51 +0200

    fájlbeolvasás

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,11 +1,14 @@
 package hu.petrik;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static List<Bejegyzes> BejegyList= new ArrayList<>();
+    public static List<Bejegyzes> bejegyList= new ArrayList<>();
     public static Scanner sc = new Scanner(System.in);
 
 
@@ -13,8 +16,8 @@ public class Main {
     public static void main(String[] args) {
 	Bejegyzes b1=new Bejegyzes("Budaházi Bence","Dolgozat");
 	Bejegyzes b2= new Bejegyzes("Györgyi Tamás", "Objektumok");
-	BejegyList.add(b1);
-	BejegyList.add(b2);
+	bejegyList.add(b1);
+	bejegyList.add(b2);
 
         int db;
         System.out.print("Adjon meg egy számmot: ");
@@ -25,11 +28,29 @@ public class Main {
             String szerzo = sc.next();
             System.out.print("Adja meg a tartalmát: ");
             String tartalom = sc.next();
-            BejegyList.add(new Bejegyzes(szerzo, tartalom));
+            bejegyList.add(new Bejegyzes(szerzo, tartalom));
+            //System.out.println(BejegyList);
 
         }
-
-
+        fileBeolvas("bejegyzesek.txt");
 
     }
+    public static void fileBeolvas(String fileNev){
+        try {
+            FileReader fr = new FileReader(fileNev);
+            BufferedReader bf = new BufferedReader(fr);
+            String sor = bf.readLine();
+            while(sor != null){
+                String[] adatok = sor.split(";");
+                bejegyList.add(new Bejegyzes(adatok[0], adatok[1]));
+                sor = bf.readLine();
+            }
+            bf.close();
+            fr.close();
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
 }
